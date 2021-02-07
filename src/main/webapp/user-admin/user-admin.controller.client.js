b63cf4b375eba1c5e3a841d841851ffdba5cd0b9
adarshreddy87 <sparkadarshreddy@gmail.com>
2021-02-06 19:51:33 -0500

    Added controller-js file for user-admin

---
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -0,0 +1,8 @@
+  var users = [
+  {userName:'adarshreddyk',password:123456,firstName:'Adarsh',lastName:'Reddy',role:'Student'},
+  {userName:'kothireddy',password:12345678,firstName:'Kothi',lastName:'Reddy',role:'Staff'},
+  {userName:'pullareddy',password:12345678,firstName:'Pulla',lastName:'Reddy',role:'Faculty'}
+  ];
+  console.log(users);
+
+
