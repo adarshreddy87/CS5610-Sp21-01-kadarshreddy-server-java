1410417bdaa5d4e6d19a34e302ac956cc3a0b36d
adarshreddy87 <sparkadarshreddy@gmail.com>
2021-02-08 13:25:49 -0500

    Added create,render functionalities in controller filr

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -1,8 +1,46 @@
   var users = [
-  {userName:'adarshreddyk',password:123456,firstName:'Adarsh',lastName:'Reddy',role:'Student'},
-  {userName:'kothireddy',password:12345678,firstName:'Kothi',lastName:'Reddy',role:'Staff'},
-  {userName:'pullareddy',password:12345678,firstName:'Pulla',lastName:'Reddy',role:'Faculty'}
-  ];
-  console.log(users);
+  {userName:'manoharreddy',password:123456,firstName:'Manohar',lastName:'Reddy',role:'Student'},
+  {userName:'haindavikrishna',password:12345678,firstName:'Haindavi',lastName:'Krishna',role:'Staff'},
+  {userName:'mahipalreddy',password:12345678,firstName:'Mahipal',lastName:'Reddy',role:'Faculty'}
+    ];
+  var tableBody = jQuery("tbody")
+  var user = [
+              {userName:'suneethareddy',
+                    password:12345678,
+                    firstName:'Suneetha',
+                    lastName:'Reddy',
+                    role:'Faculty'}]
 
+  var createUserBtn = jQuery("#create-user")
+  createUserBtn.click(function (){
+                        var user2 = [
+                                      {userName:'hi',
+                                            password:12345678,
+                                            firstName:'Suneetha',
+                                            lastName:'Reddy',
+                                            role:'Faculty'}]
+                      users.push(user2)
+                      renderUsers(users)
+
+                        })
+
+
+console.log("hi")
+console.log("hello")
+  function renderUsers(users){
+  tableBody.empty()
+  for (var i=0;i<users.length;i++){
+  var user = users[i]
+    tableBody.prepend(`<tr>
+                                   <td>${user.userName}</td>
+                                   <td>${user.password}</td>
+                                   <td>${user.firstName}</td>
+                                   <td>${user.lastName}</td>
+                                   <td>${user.role}</td>
+                                   <td><i class="icon-remove-space btn btn-danger fa fa-times fa-2x"></i></td>
+                                   <td><i class="icon-remove-space btn btn-info fa fa-pencil fa-2x"></i></td>
+                               </tr>`);
+  }
+  }
+  renderUsers(users)
 
