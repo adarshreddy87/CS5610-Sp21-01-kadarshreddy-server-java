da79f37ab61c1d737a8c40df3710db2f196cf345
adarshreddy87 <sparkadarshreddy@gmail.com>
2021-02-10 20:54:46 -0500

    Added service-client-js file and added CRUD functionalities in the controller

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -4,44 +4,79 @@ var firstNameFld
 var lastNameFld
 var roleFld
 var createUserBtn
+var updateUserBtn
 var tableBody
-var users = [
-    // {userName: 'manoharreddy', password:123456, firstName:'Manohar', lastName:'Reddy', role:'Student'},
-    // {userName: 'haindavikrishna', password:12345678, firstName:'Haindavi', lastName:'Krishna', role:'Staff'},
-    // {userName: 'mahipalreddy', password:12345678, firstName:'Mahipal', lastName:'Reddy', role:'Faculty'}
-];
-
+var userAdminService = new UserAdminServiceClient()
+var users = [];
+var selectedUser = null
+function selectUser(event){
+    var selectBtn = jQuery(event.target)
+    var theIndex = selectBtn.attr("id")
+    selectedUser = users.find(user => user._id === theIndex)
+    userNameFld.val(selectedUser.username)
+    passwordFld.val(selectedUser.password)
+    firstNameFld.val(selectedUser.firstName)
+    lastNameFld.val(selectedUser.lastName)
+    roleFld.val(selectedUser.role)
+}
 function renderUsers(users) {
     tableBody.empty()
     for (var i = 0; i < users.length; i++) {
         var user = users[i]
-        tableBody.prepend(`<tr>
-                                   <td>${user.userName}</td>
-                                   <td>${user.password}</td>
+        tableBody.append(`<tr>
+                                   <td>${user.username}</td>
+                                   <td class="hidden">${user.password}</td>
                                    <td>${user.firstName}</td>
                                    <td>${user.lastName}</td>
                                    <td>${user.role}</td>
-                                   <td><button style="white-space:nowrap" class="delete-user btn btn-danger fa fa-times fa-2x" id="${i}"></button></td>
-                                   <td><button style="white-space:nowrap" class="btn btn-info fa fa-pencil fa-2x"></button></td>
+                                   <td><i id="${i}" class="delete-user btn btn-danger fa fa-times fa-2x"></i></td>
+                                   <td><i id="${user._id}" class="select-user btn btn-info fa fa-pencil fa-2x"></i></td>
                                </tr>`
         );
     }
     jQuery(".delete-user").click(deleteUser)
+    jQuery(".select-user").click(selectUser)
 }
 // renderUsers(users)
 
-function createUser(userCreate){
-    users.push(userCreate)
-    renderUsers(users)
+function createUser(user){
+    userAdminService.createUser(user)
+        .then(function (actualUser){
+            users.push(actualUser)
+            renderUsers(users)
+        })
 }
 
 function deleteUser(event) {
     var deleteBtn = jQuery(event.target)
-    var theId = deleteBtn.attr("id")
-    users.splice(theId, 1)
-    renderUsers(users)
+    var theIndex = deleteBtn.attr("id")
+    var theId = users[theIndex]._id
+    userAdminService.deleteUser(theId)
+        .then(function (status){
+            users.splice(theIndex, 1)
+            renderUsers(users)
+        })
 }
-function init(){
+
+function updateUser(){
+    selectedUser.username = userNameFld.val()
+    selectedUser.password = passwordFld.val()
+    selectedUser.firstName = firstNameFld.val()
+    selectedUser.lastName = lastNameFld.val()
+    selectedUser.role = roleFld.val()
+    userNameFld.val("")
+    passwordFld.val("")
+    firstNameFld.val("")
+    lastNameFld.val("")
+    roleFld.val("")
+    userAdminService.updateUser(selectedUser._id,selectedUser)
+        .then(function (status){
+            var index = users.findIndex(user => user._id === selectedUser._id)
+            users[index] = selectedUser
+            renderUsers(users)
+        })
+}
+function main(){
     userNameFld=jQuery(".usernameFld")
     passwordFld=jQuery(".passwordFld")
     firstNameFld=jQuery(".firstnameFld")
@@ -49,13 +84,16 @@ function init(){
     roleFld=jQuery(".roleFld")
     tableBody= jQuery("tbody")
     createUserBtn = jQuery("#create-user")
+    updateUserBtn = jQuery("#update-user")
+    updateUserBtn.click(updateUser)
     createUserBtn.click(function (){
-            createUser({
-                userName:userNameFld.val(),
-                password:passwordFld.val(),
-                firstName:firstNameFld.val(),
-                lastName:lastNameFld.val(),
-                role:roleFld.val()
+
+        createUser({
+            username:userNameFld.val(),
+            password:passwordFld.val(),
+            firstName:firstNameFld.val(),
+            lastName:lastNameFld.val(),
+            role:roleFld.val()
             })
             userNameFld.val("")
             passwordFld.val("")
@@ -64,6 +102,11 @@ function init(){
             roleFld.val("")
         }
     )
+    userAdminService.findAllUsers()
+        .then(function (actualUsersFromServer){
+            users = actualUsersFromServer
+            renderUsers(users)
+        })
 }
 
-jQuery(init)
+jQuery(main)
