c1c1502cead5bdc6bb1956397d214d11195e7d9e
adarshreddy87 <sparkadarshreddy@gmail.com>
2021-03-21 14:47:57 -0400

    Created WidgetController class

---
+++ src/main/java/com/example/CS5610Sp2101kadarshreddyserverjava/controllers/WidgetController.java
@@ -0,0 +1,46 @@
+package com.example.CS5610Sp2101kadarshreddyserverjava.controllers;
+
+import com.example.CS5610Sp2101kadarshreddyserverjava.models.Widget;
+import com.example.CS5610Sp2101kadarshreddyserverjava.services.WidgetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class WidgetController {
+    @Autowired
+    WidgetService service;
+
+    @GetMapping("/api/widgets")
+    public List<Widget> findAllWidgets(){
+        return service.findAllWidgets();
+    }
+
+    @GetMapping("/api/topic/{tid}/widgets")
+    public List<Widget> findALlWidgetsForTopic(
+            @PathVariable("tid") String topicId){
+        return service.findAllWidgetsForTopic(topicId);
+    }
+
+    @PostMapping("/api/topics/{tid}/widgets")
+    public Widget createWidgetForTopic(
+            @PathVariable("tid") String topicId,
+            @RequestBody Widget widget){
+        return service.createWidgetForTopic(topicId,widget);
+    }
+
+    @PutMapping("/api/widgets/{wid}")
+    public Integer updateWidget(
+            @PathVariable("wid") Long id,
+            @RequestBody Widget widget){
+        return service.updateWidget(id,widget);
+    }
+
+    @DeleteMapping("/api/widgets/{wid}")
+    public Integer deleteWidget(
+            @PathVariable("wid")Long id){
+        return service.deleteWidget(id);
+    }
+}
