c542b1fb32ea6f9dfdbfcca81e6ed15ee22c8fe4
adarshreddy87 <sparkadarshreddy@gmail.com>
2021-04-02 18:01:57 -0400

    Added few more attributes to Widget model

--- src/main/java/com/example/CS5610Sp2101kadarshreddyserverjava/models/Widget.java
+++ src/main/java/com/example/CS5610Sp2101kadarshreddyserverjava/models/Widget.java
@@ -16,6 +16,69 @@ public class Widget {
     private Integer height;
     private Integer width;
     private String name;
+    private String src;
+    private Boolean ordered;
+    private Integer widgetOrder;
+    private String url;
+    private String cssClass;
+    private String style;
+    private String value;
+
+    public String getSrc() {
+        return src;
+    }
+
+    public void setSrc(String src) {
+        this.src = src;
+    }
+
+    public Boolean getOrdered() {
+        return ordered;
+    }
+
+    public void setOrdered(Boolean ordered) {
+        this.ordered = ordered;
+    }
+
+    public Integer getWidgetOrder() {
+        return widgetOrder;
+    }
+
+    public void setWidgetOrder(Integer widgetOrder) {
+        this.widgetOrder = widgetOrder;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCssClass() {
+        return cssClass;
+    }
+
+    public void setCssClass(String cssClass) {
+        this.cssClass = cssClass;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 
     public Integer getHeight() {
         return height;
